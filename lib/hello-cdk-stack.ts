503dd04112cfa5b925e99827500f50db7950faed
piaand <pia.m.andersin@gmail.com>
2022-09-04 17:32:48 +0300

    first stack template

---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,21 @@
+import * as cdk from 'aws-cdk-lib';
+import { aws_s3 as s3} from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+// import * as sqs from 'aws-cdk-lib/aws-sqs';
+
+export class HelloCdkStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+
+    // example resource
+    // const queue = new sqs.Queue(this, 'HelloCdkQueue', {
+    //   visibilityTimeout: cdk.Duration.seconds(300)
+    // });
+
+    const myFirstBucket = new s3.Bucket(this, 'MyFirstBucket', {
+      versioned: true
+    });
+  }
+}
